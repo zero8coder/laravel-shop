3438df23ef0c228bd9e83f24d6ad0c88961ddfb6
ZeroXGod <s812419396@gmail.com>
2020-06-03 03:04:41 +0000

    邮箱验证功能

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,20 +5,20 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('请确认你的邮箱地址') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('一个新的验证链接已经发送到您的电子邮件地址') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    {{ __('在继续之前，请检查您的电子邮件的验证链接。') }}
+                    {{ __(' 如果你没有收到邮件') }},
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('单击此处请求另一个') }}</button>.
                     </form>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', 'PagesController@root')->name('root');
-
-Auth::routes();
 
