9746afa36d77685c50a3cc4abac805585ca5e1e0
ZeroXGod <s812419396@gmail.com>
2020-06-08 07:49:30 +0000

    用户界面 - 商品详情页

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 Auth::routes();
 Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
