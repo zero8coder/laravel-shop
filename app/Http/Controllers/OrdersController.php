16342a00b6f38a56955d6ffa158349851bc92888
vagrant <vagrant@homestead>
2020-06-15 11:44:42 +0000

    关闭未支付订单

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -9,6 +9,7 @@ use App\Models\UserAddress;
 use App\Models\Order;
 use Carbon\Carbon;
 use App\Exceptions\InvalidRequestException;
+use App\Jobs\CloseOrder;
 
 class OrdersController extends Controller
 {
@@ -63,6 +64,7 @@ class OrdersController extends Controller
             // 将下单的产品从购物车中移除
             $skuIds = collect($items)->pluck('sku_id');
             $user->cartItems()->whereIn('product_sku_id', $skuIds)->delete();
+            $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
 
             return $order;
         });
