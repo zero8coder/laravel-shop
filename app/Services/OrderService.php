1c311c4b0262332329bd3117b3bc95b0dd4fad80
ZeroXGod <s812419396@gmail.com>
2020-06-17 10:00:40 +0000

    订单支付

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -10,24 +10,21 @@ use App\Exceptions\InvalidRequestException;
 use App\Jobs\CloseOrder;
 use Carbon\Carbon;
 
-
 class OrderService
 {
-    public function store(User $user, UserAddress, $address, $remark, $items)
+    public function store(User $user, UserAddress $address, $remark, $items)
     {
-          // 开启一个事务
+        // 开启一个数据库事务
         $order = \DB::transaction(function () use ($user, $address, $remark, $items) {
-
-            // 更细此地址的最后使用时间
+            // 更新此地址的最后使用时间
             $address->update(['last_used_at' => Carbon::now()]);
             // 创建一个订单
-            $order = new Order([
+            $order   = new Order([
                 'address'      => [ // 将地址信息放入订单中
-                                    'address'       => $address->full_address,
-                                    'zip'           => $address->zip,
-                                    'contact_name'  => $address->contact_name,
-                                    'contact_phone' => $address->contact_pohone,
-
+                    'address'       => $address->full_address,
+                    'zip'           => $address->zip,
+                    'contact_name'  => $address->contact_name,
+                    'contact_phone' => $address->contact_phone,
                 ],
                 'remark'       => $remark,
                 'total_amount' => 0,
@@ -40,8 +37,8 @@ class OrderService
             $totalAmount = 0;
             // 遍历用户提交的 SKU
             foreach ($items as $data) {
-                $sku = ProductSku::find($data['sku_id']);
-                // 创建一个 OrderItem 并直接与当前订单失联
+                $sku  = ProductSku::find($data['sku_id']);
+                // 创建一个 OrderItem 并直接与当前订单关联
                 $item = $order->items()->make([
                     'amount' => $data['amount'],
                     'price'  => $sku->price,
@@ -54,19 +51,19 @@ class OrderService
                     throw new InvalidRequestException('该商品库存不足');
                 }
             }
-
             // 更新订单总金额
             $order->update(['total_amount' => $totalAmount]);
 
-            // 将下单的产品从购物车中移除
-            $skuIds = collect($items)->pluck('sku_id');
+            // 将下单的商品从购物车中移除
+            $skuIds = collect($items)->pluck('sku_id')->all();
             app(CartService::class)->remove($skuIds);
 
             return $order;
         });
+
+        // 这里我们直接使用 dispatch 函数
         dispatch(new CloseOrder($order, config('app.order_ttl')));
 
         return $order;
-
     }
 }
