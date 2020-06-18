a50aceee4782727b2441bf06c0a7a58385da2ca1
ZeroXGod <s812419396@gmail.com>
2020-06-18 02:41:00 +0000

    管理后台 - 订单详情

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -7,17 +7,13 @@ use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
+use Encore\Admin\Layout\Content;
 
 class OrdersController extends AdminController
 {
 
     protected $title = '订单';
 
-    /**
-     * Make a grid builder.
-     *
-     * @return Grid
-     */
     protected function grid()
     {
         $grid = new Grid(new Order());
@@ -53,63 +49,13 @@ class OrdersController extends AdminController
         return $grid;
     }
 
-    /**
-     * Make a show builder.
-     *
-     * @param mixed $id
-     * @return Show
-     */
-    protected function detail($id)
+
+    public function show($id, Content $content)
     {
-        $show = new Show(Order::findOrFail($id));
+        return $content
+            ->header('查看订单')
+            // body 方法可以接受 Laravel 的视图作为参数
+            ->body(view('admin.orders.show', ['order' => Order::find($id)]));
 
-        $show->field('id', __('Id'));
-        $show->field('no', __('No'));
-        $show->field('user_id', __('User id'));
-        $show->field('address', __('Address'));
-        $show->field('total_amount', __('Total amount'));
-        $show->field('remark', __('Remark'));
-        $show->field('paid_at', __('Paid at'));
-        $show->field('payment_method', __('Payment method'));
-        $show->field('payment_no', __('Payment no'));
-        $show->field('refund_status', __('Refund status'));
-        $show->field('refund_no', __('Refund no'));
-        $show->field('closed', __('Closed'));
-        $show->field('reviewed', __('Reviewed'));
-        $show->field('ship_status', __('Ship status'));
-        $show->field('ship_data', __('Ship data'));
-        $show->field('extra', __('Extra'));
-        $show->field('created_at', __('Created at'));
-        $show->field('updated_at', __('Updated at'));
-
-        return $show;
-    }
-
-    /**
-     * Make a form builder.
-     *
-     * @return Form
-     */
-    protected function form()
-    {
-        $form = new Form(new Order());
-
-        $form->text('no', __('No'));
-        $form->number('user_id', __('User id'));
-        $form->textarea('address', __('Address'));
-        $form->decimal('total_amount', __('Total amount'));
-        $form->textarea('remark', __('Remark'));
-        $form->datetime('paid_at', __('Paid at'))->default(date('Y-m-d H:i:s'));
-        $form->text('payment_method', __('Payment method'));
-        $form->text('payment_no', __('Payment no'));
-        $form->text('refund_status', __('Refund status'))->default('pending');
-        $form->text('refund_no', __('Refund no'));
-        $form->switch('closed', __('Closed'));
-        $form->switch('reviewed', __('Reviewed'));
-        $form->text('ship_status', __('Ship status'))->default('pending');
-        $form->textarea('ship_data', __('Ship data'));
-        $form->textarea('extra', __('Extra'));
-
-        return $form;
     }
 }
