24b56cf1edeee573f08ba03b5db982c749cc06ce
ZeroXGod <s812419396@gmail.com>
2020-06-19 09:31:54 +0000

    管理后台 - 拒绝退款

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -2,14 +2,14 @@
 
 namespace App\Admin\Controllers;
 
+use App\Exceptions\InvalidRequestException;
 use App\Models\Order;
 use Encore\Admin\Controllers\AdminController;
-use Encore\Admin\Form;
 use Encore\Admin\Grid;
-use Encore\Admin\Show;
 use Encore\Admin\Layout\Content;
 use Illuminate\Http\Request;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use App\Http\Requests\Admin\HandleRefundRequest;
 
 class OrdersController extends AdminController
 {
@@ -93,4 +93,29 @@ class OrdersController extends AdminController
         // 返回上一页
         return redirect()->back();
     }
+
+    public function handleRefund(Order $order, HandleRefundRequest $request)
+    {
+        // 判断订单状态是否正确
+        if ($order->refund_status !== Order::REFUND_STATUS_APPLIED) {
+            throw new InvalidRequestException('订单状态不正确');
+        }
+        // 是否同意退款
+        if ($request->input('agree')) {
+            // 同意退款的逻辑这里先留空
+            // todo
+        } else {
+            // 将拒绝退款理由放到订单的 extra 字段中
+            $extra = $order->extra ?: [];
+            $extra['refund_disagree_reason'] = $request->input('reason');
+            // 将订单的退款状态改为未退款
+            $order->update([
+               'refund_status' => Order::REFUND_STATUS_PENDING,
+               'extra' => $extra,
+            ]);
+        }
+
+        return $order;
+    }
+
 }
