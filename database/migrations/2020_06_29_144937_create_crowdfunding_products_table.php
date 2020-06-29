010a63baf73b900a805717aba16541dca1da5389
ZeroXGod <s812419396@gmail.com>
2020-06-29 07:23:14 +0000

    初始化众筹模型

---
+++ database/migrations/2020_06_29_144937_create_crowdfunding_products_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCrowdfundingProductsTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('crowdfunding_products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('product_id')->comment('商品id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->decimal('target_amount', 10, 2)->comment('众筹目标金额');
+            $table->decimal('total_amount', 10, 2)->default(0)->comment('当前已筹金额');
+            $table->unsignedInteger('user_count')->default(0)->comment('当前参与众筹用户数');
+            $table->dateTime('end_at')->comment('众筹结束时间');
+            $table->string('status')->default(\App\Models\CrowdfundingProduct::STATUS_FUNDING)->comment('当前筹款的状态');
+        });
+    }
+
+
+    public function down()
+    {
+        Schema::dropIfExists('crowdfunding_products');
+    }
+}
---
+++ database/migrations/2020_06_29_151206_products_add_type.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ProductsAddType extends Migration
+{
+
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('type')->after('id')->default(\App\Models\Product::TYPE_NORMAL)->index()->comment('商品类型');
+        });
+    }
+
+
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('type');
+        });
+    }
+}
